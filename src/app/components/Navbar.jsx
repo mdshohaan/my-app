7911fe8fe600251e845437f8e43f1bdc36188673
mdshohaan <irfanulislamdu@gmail.com>
2025-02-07 22:25:03 +0600

    first commit

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,29 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function NavBar() {
+  const pathname = usePathname();
+  console.log(pathname, pathname.includes("dashboard"));
+  if (!pathname.includes("dashboard")) {
+    return (
+      <div>
+        <nav className="flex justify-center">
+          <ul className="flex justify-between w-1/2">
+            <Link href="/">
+              <li>Home</li>
+            </Link>
+            <Link href="/services">
+              <li>Services</li>
+            </Link>
+            <Link href="/about">
+              <li>About</li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
+}
